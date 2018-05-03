4725dbfdfb43ffaea97bdd36f8ae1bd1ad9799a8
Jacob Bare <jacob@limit0.io>
2018-05-03 09:55:59 -0500

    Connect to redis/mongo and gracefully shutdown

---
+++ src/errors.js
@@ -0,0 +1,10 @@
+module.exports = {
+  /**
+   * @todo Report to NewRelic.
+   * @param {Error} error
+   */
+  report(error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const pkg = require('../package.json');
 const app = require('./express');
+const mongoose = require('./mongoose');
+const redis = require('./redis');
+const errors = require('./errors');
 
 const { HTTP_PORT } = process.env;
 
@@ -8,4 +11,27 @@ const httpServer = app.listen(HTTP_PORT, () => {
   process.stdout.write(`HTTP server for '${pkg.name}' listening on port ${HTTP_PORT}\n`);
 });
 
-module.exports = { httpServer };
+// Shutdown app gracefully.
+function handleExit(options, err) {
+  if (options.cleanup) {
+    const actions = [httpServer.close, mongoose.disconnect, redis.quit];
+    actions.forEach((close, i) => {
+      try {
+        close(() => {
+          if (i === actions.length - 1) process.exit();
+        });
+      } catch (e) {
+        if (i === actions.length - 1) process.exit();
+      }
+    });
+  }
+  if (err) errors.report(err);
+  if (options.exit) process.exit();
+}
+
+process.on('exit', handleExit.bind(null, { cleanup: true }));
+process.on('SIGINT', handleExit.bind(null, { exit: true }));
+process.on('SIGTERM', handleExit.bind(null, { exit: true }));
+process.on('uncaughtException', handleExit.bind(null, { exit: true }));
+
+module.exports = { httpServer, mongoose, redis };
