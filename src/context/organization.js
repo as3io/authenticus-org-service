6e46bb10cd304d82ebe4695b3ff138b9dae5e8c4
Jacob Bare <jacob@limit0.io>
2018-05-07 12:49:31 -0500

    Add owning user id to org

--- src/context/organization.js
+++ src/context/organization.js
@@ -14,7 +14,7 @@ class OrganizationContext {
   async load() {
     const { id } = this;
     if (!id) throw new Error('No organization ID was provided.');
-    const org = await Organization.findOne({ _id: id }, { _id: 1 });
+    const org = await Organization.findOne({ _id: id }, { _id: 1, owningUserId: 1 });
     if (!org) throw new Error(`No organization found for ID '${id}'`);
     return org;
   }
--- src/schema/organization.js
+++ src/schema/organization.js
@@ -1,4 +1,5 @@
 const { Schema } = require('mongoose');
+const User = require('../models/user');
 const pushIdPlugin = require('../plugins/push-id');
 const sluggablePlugin = require('../plugins/sluggable');
 
@@ -11,6 +12,18 @@ const schema = new Schema({
   description: {
     type: String,
     trim: true,
+  },
+  owningUserId: {
+    type: String,
+    required: true,
+    validate: {
+      async validator(v) {
+        const doc = await User.findOne({ _id: v }, { _id: 1 });
+        if (doc) return true;
+        return false;
+      },
+      message: 'No user found for ID {VALUE}',
+    },
   },
 }, {
   timestamps: true,
