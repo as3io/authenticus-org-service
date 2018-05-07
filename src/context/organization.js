430aa71a7e6cc2fdb98931aceae52cdf50600f81
Jacob Bare <jacob@limit0.io>
2018-05-07 12:32:25 -0500

    Create org context

---
+++ src/context/organization.js
@@ -0,0 +1,23 @@
+const Organization = require('../models/organization');
+
+class OrganizationContext {
+  /**
+   * @param {string} id The organization ID.
+   */
+  constructor(id) {
+    this.id = id;
+  }
+
+  /**
+   *
+   */
+  async load() {
+    const { id } = this;
+    if (!id) throw new Error('No organization ID was provided.');
+    const org = await Organization.findOne({ _id: id }, { _id: 1 });
+    if (!org) throw new Error(`No organization found for ID '${id}'`);
+    return org;
+  }
+}
+
+module.exports = OrganizationContext;
--- src/routes/graph.js
+++ src/routes/graph.js
@@ -3,16 +3,18 @@ const bodyParser = require('body-parser');
 const { noCache } = require('helmet');
 const { graphqlExpress } = require('apollo-server-express');
 const schema = require('../graph/schema');
+const OrganizationContext = require('../context/organization');
 
 const router = Router();
 
 router.use(
   noCache(),
   bodyParser.json(),
-  graphqlExpress(() => ({
-    schema,
-    context: {},
-  })),
+  graphqlExpress((req) => {
+    const organization = new OrganizationContext(req.get('X-Organization-ID'));
+    const context = { organization };
+    return { schema, context };
+  }),
 );
 
 module.exports = router;
