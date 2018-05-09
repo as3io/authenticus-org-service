1d27118b242ca944ca3d7fd2ae61919cffb48820
Jacob Bare <jacob@limit0.io>
2018-05-09 08:57:55 -0500

    Add change password and update profile

--- src/graph/resolvers/user.js
+++ src/graph/resolvers/user.js
@@ -1,5 +1,14 @@
 const UserService = require('../../services/user');
 
+/**
+ * @todo This function should likely move to the user service.
+ */
+const validatePassword = (value, confirm) => {
+  if (!value || !confirm) throw new Error('You must provide and confirm your password.');
+  if (value.length < 6) throw new Error('Passwords must be at least six characters long.');
+  if (value !== confirm) throw new Error('The password does not match the confirmation password.');
+};
+
 module.exports = {
   /**
    *
@@ -43,6 +52,35 @@ module.exports = {
       const { id, uid } = auth.session;
       await UserService.deleteSession(id, uid);
       return 'ok';
+    },
+
+    /**
+     *
+     */
+    changeUserPassword: async (root, { input }, { auth }) => {
+      auth.check();
+      const { user } = auth;
+      const { id, value, confirm } = input;
+      if (user.id.valueOf() === id) {
+        validatePassword(value, confirm);
+        const record = await UserService.findById(id);
+        if (!record) throw new Error(`No user record found for ID ${id}.`);
+        record.password = value;
+        // @todo Invalidate all existing sessions for this user.
+        return record.save();
+      }
+      throw new Error('Only administrators can change passwords for other users.');
+    },
+
+    /**
+     *
+     */
+    updateCurrentUserProfile: async (root, { input }, { auth }) => {
+      auth.check();
+      const { givenName, familyName } = input;
+      const { user } = auth;
+      user.set({ givenName, familyName });
+      return user.save();
     },
   },
 };
