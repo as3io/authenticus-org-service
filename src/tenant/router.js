d0c22bc03481b6d44bb781ba9cc9d324e6f8fec2
Jacob Bare <jacob@limit0.io>
2018-05-10 10:41:34 -0500

    Load tenant for all subroutes of tenant router

--- src/tenant/router.js
+++ src/tenant/router.js
@@ -1,9 +1,23 @@
+const Promise = require('bluebird');
 const { Router } = require('express');
+const Tenant = require('../core/models/tenant');
 
-const router = Router();
+const { assign } = Object;
+const handleAsync = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
 
-router.get('/', (req, res) => {
-  res.send({});
+const router = Router({ mergeParams: true });
+
+const loadTenant = handleAsync(async (req, res, next) => {
+  const { tenantId } = req.params;
+  if (!tenantId) throw new Error('No tenant ID was provided with the request.');
+  const tenant = await Tenant.findOne({ _id: tenantId });
+  if (!tenant) throw new Error('No tenant found for the provided ID.');
+  assign(req, { tenant });
+  next();
+});
+
+router.get('/', loadTenant, (req, res) => {
+  res.send(req.tenant);
 });
 
 module.exports = router;
